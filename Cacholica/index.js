d3820eccdaa3ccdd0ccff01de9a5d18186135183
stcalica <stcalica@gmail.com>
2015-06-01 20:44:22 -0700

    added some canvas stuff

--- Cacholica/index.js
+++ Cacholica/index.js
@@ -15,20 +15,44 @@ $(document).ready(function(){
   			//Redraw & reposition content
   			var x = c.width();
   			var y = c.height();  			
-  			ct.font = "20px Calibri";
   			
-  			ct.fillStyle = "#DDDDDD"; //black
-  			ct.fillRect( 0, 0, x, y); //fill the canvas
-  			
-  			var resizeText = "Canvas width: "+c.width()+"px";
-  			ct.textAlign = "center";
-  			ct.fillStyle = "#333333"; //white
-	  		ct.fillText(resizeText, (x/2), (y/2) );
+  	//		ct.fillStyle = "#DDDDDD"; //black
+  	//		ct.fillRect( 0, 0, x, y); //fill the canvas
+
+  			//ct.font = "20px Calibri";
+  			//var resizeText = "Canvas width: "+c.width()+"px";
+  			//ct.textAlign = "center";
+  			//ct.fillStyle = "#333333"; //white
+	  	  //ct.fillText(resizeText, (x/2), (y/2) );
 		}
-		
+
+    function addImage(){
+      var canvas = $("#socialWall");
+      var ctx = canvas.get(0).getContext('2d');
+      fabric.Image.fromURL("./backgroundflowers.jpg", function(img){
+          //  var pattern = ctx.createPattern(img, "repeat");
+            //ctx.fillStyle = pattern;
+            canvas.add(img);
+       });
+
+		}
+
+    function drawOnImage(){
+
+
+
+    }
+
+    function shareImage(){
+
+
+
+    }
 		//Initial call
 		respondCanvas();
- 
+    addImage();
+    //drawOnImage();
+    //shareImage() should be attached to a button with an onclick listner
 
 
 });
